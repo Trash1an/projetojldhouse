7187e84541a9b6647dd0ef0b0f5c36019ab0e7f4
Trash1an <hian.alcantra@gmail.com>
2023-09-04 10:55:55 -0300

    atualização

--- script.js
+++ script.js
@@ -13,3 +13,45 @@ window.addEventListener("scroll", function(){
 
 
 AOS.init();
+
+const overlay = document.getElementById("overlay");
+const overlayImage = document.querySelector(".overlay-image");
+const closeBtn = document.getElementById("close-btn");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+
+const trabalhos = document.querySelectorAll(".trabalho img");
+let currentIndex = 0;
+
+function showImage(index) {
+    if (index >= 0 && index < trabalhos.length) {
+        const selectedImageSrc = trabalhos[index].src;
+        overlayImage.src = selectedImageSrc;
+        currentIndex = index;
+    }
+}
+
+trabalhos.forEach((trabalho, index) => {
+    trabalho.addEventListener("click", () => {
+        showImage(index);
+        overlay.style.display = "block";
+    });
+});
+
+overlay.addEventListener("click", (e) => {
+    if (e.target === overlay) {
+        overlay.style.display = "none";
+    }
+});
+
+closeBtn.addEventListener("click", () => {
+    overlay.style.display = "none";
+});
+
+prevBtn.addEventListener("click", () => {
+    showImage(currentIndex - 1);
+});
+
+nextBtn.addEventListener("click", () => {
+    showImage(currentIndex + 1);
+});
